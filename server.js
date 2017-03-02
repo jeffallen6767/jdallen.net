6e9cb85804be321633081d3e1662d94646a9ce29
Jeffrey Allen <1063272+jeffallen6767@users.noreply.github.com>
2017-03-01 19:19:06 -0800

    npm scripts and simplify dir structure

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("server.js");
+var express = require('express');
+var app = express();
+var port = 6767;
+
+app.use(express.static('docs'));
+
+app.listen(port, function () {
+  console.log('dev server listening on port ' + port);
+});
